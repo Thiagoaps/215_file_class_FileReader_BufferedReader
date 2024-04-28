387d85759aadb1c0001a9899d0883620fae4727a
Thiago Antonio <thiagoapsx@gmail.com>
2024-04-28 20:35:04 -0300

    file class, changing to try with resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		File path = new File("C:\\temp\\in.txt");
-		BufferedReader br = null;
-		FileReader fr = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr); 
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			String line = br.readLine();
 
@@ -25,16 +22,6 @@ public class Program {
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null)
-					br.close();
-
-				if (fr != null)
-					fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
